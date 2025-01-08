ca1c1848dfdcfb17f2ba1b56203a29f719a8c3c0
kwin64 <kwin649011@gmail.com>
2025-01-09 00:00:46 +0300

    fix: auth comment

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import blogsRouter from './routes/blogsRouter';
 import testsRouter from './routes/testsRouter';
+import postsRouter from './routes/postsRouter';
 import SETTINGS from './utils/constants/settings';
 export const app = express();
 
@@ -12,4 +13,5 @@ app.get('/', (req, res) => {
   res.status(200).send('server start');
 });
 app.use(SETTINGS.PATH.BLOGS, blogsRouter);
+app.use(SETTINGS.PATH.POSTS, postsRouter);
 app.use(SETTINGS.PATH.TESTS, testsRouter);
--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import postsController from '../controllers/postsController';
+import authMiddleware from '../middlewares/authMiddleware';
 
-export const postsRouter = Router({});
+const postsRouter = Router({});
 postsRouter.get('/blogs', postsController.allPosts);
 postsRouter.get('/blogs/:id', postsController.allPosts);
-postsRouter.post('/blogs', postsController.allPosts); //auth path
-postsRouter.put('/blogs/:id', postsController.allPosts); //auth path
-postsRouter.delete('/blogs/:id', postsController.allPosts); //auth path
+postsRouter.post('/blogs', authMiddleware, postsController.allPosts);
+postsRouter.put('/blogs/:id', authMiddleware, postsController.allPosts);
+postsRouter.delete('/blogs/:id', authMiddleware, postsController.allPosts);
+
+export default postsRouter;
