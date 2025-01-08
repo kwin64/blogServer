82a61718f055b0d311333304fd72fb20a9c84cdd
kwin64 <kwin649011@gmail.com>
2025-01-09 01:24:57 +0300

    fix: change status code for different valuesin auth

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,36 +1,45 @@
 import { NextFunction, Request, Response } from 'express';
 import { HTTP_STATUSES } from '../utils/constants/httpStatuses';
 import SETTINGS from '../utils/constants/settings';
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const authUsername = SETTINGS.AUTH_USERNAME;
-  const authPassword = SETTINGS.AUTH_PASSWORD;
-  const authHeader = req.headers.authorization;
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { AUTH_USERNAME, AUTH_PASSWORD } = SETTINGS;
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    res
-      .status(HTTP_STATUSES.UNAUTHORIZED)
-      .json({ message: 'Authorization header is missing' });
-    return;
-  }
+    if (!authHeader) {
+      res
+        .status(HTTP_STATUSES.UNAUTHORIZED)
+        .json({ message: 'Authorization header is missing' });
+      return;
+    }
 
-  if (authHeader.split(' ')[0] !== 'Basic') {
-    res
-      .status(HTTP_STATUSES.UNAUTHORIZED)
-      .json({ message: 'Invalid authorization format' });
-    return;
-  }
+    if (authHeader.split(' ')[0] !== 'Basic') {
+      res
+        .status(HTTP_STATUSES.UNAUTHORIZED)
+        .json({ message: 'Invalid authorization format' });
+      return;
+    }
 
-  const decoded = Buffer.from(authHeader.split(' ')[1], 'base64').toString(
-    'utf-8'
-  );
-  if (
-    decoded.split(':')[0] !== authUsername ||
-    decoded.split(':')[1] !== authPassword
-  ) {
-    res
-      .status(HTTP_STATUSES.FORBIDDEN)
-      .json({ message: 'The authorization header is invalid' });
-    return;
+    const decoded = Buffer.from(authHeader.split(' ')[1], 'base64').toString(
+      'utf-8'
+    );
+    if (
+      decoded.split(':')[0] !== AUTH_USERNAME ||
+      decoded.split(':')[1] !== AUTH_PASSWORD
+    ) {
+      res
+        .status(HTTP_STATUSES.UNAUTHORIZED)
+        .json({ message: 'The authorization header is invalid' });
+      return;
+    }
+  } catch (error) {
+    //когда выводить 500
+    console.error('auth middleware error:', error);
+    res.sendStatus(HTTP_STATUSES.INTERNAL_SERVER_ERROR);
   }
 
   next();
