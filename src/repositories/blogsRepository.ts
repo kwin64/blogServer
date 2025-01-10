2f38a518a1f2ef864dc342f8e21e39f4e90490c8
kwin64 <kwin649011@gmail.com>
2025-01-11 00:38:34 +0300

    fix: try catch

--- src/repositories/blogsRepository.ts
+++ src/repositories/blogsRepository.ts
@@ -3,65 +3,40 @@ import { blogType } from '../DB/DB.types';
 
 const blogsRepository = {
   async getBlogs(): Promise<blogType[]> {
-    try {
-      const allBlogs = await DB.blogs;
-      return allBlogs;
-    } catch (error) {
-      console.error('Error get blogs with DB:', error);
-      throw new Error('Database: get blogs failed');
-    }
+    const allBlogs = await DB.blogs;
+    return allBlogs;
   },
   async create(blog: blogType): Promise<blogType> {
-    try {
-      DB.blogs.push(blog);
-      return blog;
-    } catch (error) {
-      console.error('Error set blog in DB:', error);
-      throw new Error('Database: set blog failed');
-    }
+    DB.blogs.push(blog);
+    return blog;
   },
   async getBlog(id: string): Promise<blogType | null> {
-    try {
-      const foundedBlog = await DB.blogs.find((blog) => blog.id === id);
-      if (foundedBlog) {
-        return foundedBlog;
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.error('Error get blog with DB:', error);
-      throw new Error('Database: get blog failed');
+    const foundedBlog = await DB.blogs.find((blog) => blog.id === id);
+    if (foundedBlog) {
+      return foundedBlog;
+    } else {
+      return null;
     }
   },
   async change(blog: blogType): Promise<blogType | null> {
-    try {
-      let foundedBlog: blogType | undefined = DB.blogs.find(
-        (item) => item.id === blog.id
-      );
-      if (foundedBlog) {
-        foundedBlog.name = blog.name;
-        foundedBlog.description = blog.description;
-        foundedBlog.websiteUrl = blog.websiteUrl;
-        return foundedBlog;
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.error('Error change blog in DB:', error);
-      throw new Error('Database: change blog failed');
+    let foundedBlog: blogType | undefined = DB.blogs.find(
+      (item) => item.id === blog.id
+    );
+    if (foundedBlog) {
+      foundedBlog.name = blog.name;
+      foundedBlog.description = blog.description;
+      foundedBlog.websiteUrl = blog.websiteUrl;
+      return foundedBlog;
+    } else {
+      return null;
     }
   },
   async delete(id: string) {
-    try {
-      const index = await DB.blogs.findIndex((blog) => blog.id === id);
-      if (index === -1) {
-        return null;
-      }
-      return DB.blogs.splice(index, 1);
-    } catch (error) {
-      console.error('Error delete blog with DB:', error);
-      throw new Error('Database: delete blog failed');
+    const index = await DB.blogs.findIndex((blog) => blog.id === id);
+    if (index === -1) {
+      return null;
     }
+    return DB.blogs.splice(index, 1);
   },
 };
 
--- src/repositories/postsRepository.ts
+++ src/repositories/postsRepository.ts
@@ -3,33 +3,18 @@ import { postType } from '../DB/DB.types';
 
 const postsRepository = {
   async getPosts(): Promise<postType[]> {
-    try {
-      return await DB.posts;
-    } catch (error) {
-      console.error('Error get posts with DB:', error);
-      throw new Error('Database: get posts failed');
-    }
+    return await DB.posts;
   },
   async create(post: postType): Promise<postType> {
-    try {
-      DB.posts.push(post);
-      return post;
-    } catch (error) {
-      console.error('Error set post in DB:', error);
-      throw new Error('Database: set post failed');
-    }
+    DB.posts.push(post);
+    return post;
   },
   async getPost(id: string): Promise<postType | null> {
-    try {
-      const foundedPost = await DB.posts.find((post) => post.id === id);
-      if (foundedPost) {
-        return foundedPost;
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.error('Error get post with DB:', error);
-      throw new Error('Database: get post failed');
+    const foundedPost = await DB.posts.find((post) => post.id === id);
+    if (foundedPost) {
+      return foundedPost;
+    } else {
+      return null;
     }
   },
   async delete(id: string) {
@@ -43,21 +28,16 @@ const postsRepository = {
     DB.posts = await DB.posts.filter((post) => post.blogId !== blogId);
   },
   async change(post: postType): Promise<postType | null> {
-    try {
-      const newPost: postType | undefined = DB.posts.find(
-        (item) => item.id === post.id
-      );
-      if (newPost) {
-        newPost.title = post.title;
-        newPost.shortDescription = post.shortDescription;
-        newPost.content = post.content;
-        return newPost;
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.error('Error change post in DB:', error);
-      throw new Error('Database: change post failed');
+    const newPost: postType | undefined = DB.posts.find(
+      (item) => item.id === post.id
+    );
+    if (newPost) {
+      newPost.title = post.title;
+      newPost.shortDescription = post.shortDescription;
+      newPost.content = post.content;
+      return newPost;
+    } else {
+      return null;
     }
   },
 };
