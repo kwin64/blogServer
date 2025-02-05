1c6f0f4db8bd557e8b614688eab35d0e9c8bd3c9
kwin64 <kwin649011@gmail.com>
2025-02-05 23:08:45 +0300

    fix: clear logs

--- src/repositories/queries/commentQueryRepository.ts
+++ src/repositories/queries/commentQueryRepository.ts
@@ -10,8 +10,6 @@ const commentQueryRepository = {
       throw ApiError.badRequest('Invalid comment ID format');
     }
 
-    console.log('67a1230ac367328d02bb27c4');
-    
     const comment = await Comment.findOne({ _id }).lean<CommentDocument>();
 
     if (!comment) {
