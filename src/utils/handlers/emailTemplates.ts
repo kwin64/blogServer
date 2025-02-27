ec4dbe25d731c8eb3aab4eef502f686dbf99b488
kwin6490 <kwin6490bvb@gmail.com>
2025-02-27 18:07:50 +0300

    fix: example template mail

--- src/utils/handlers/emailTemplates.ts
+++ src/utils/handlers/emailTemplates.ts
@@ -2,8 +2,8 @@ const emailTemplates = {
   registrationConfirmationEmail(code: string) {
     return ` <h1>Thank for your registration</h1>
              <p>To finish registration please follow the link below:<br>
-                <a href='https://blog-server-nine-theta.vercel.app/auth/registration-confirmation?code=${code}'>complete registration</a>
-             </p>`;
+                <a href='https://7abe02b4dcf9060ff42d9a2e3233cd31.serveo.net/auth/registration-confirmation?code=${code}'>complete registration</a>
+             </p>`
   },
 };
 
