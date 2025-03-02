2b45c75cb537b3a93270a73154a6637aae16dab5
kwin6490 <kwin6490bvb@gmail.com>
2025-03-02 13:39:51 +0300

    fix: change email template and delete docker files

--- src/utils/handlers/emailTemplates.ts
+++ src/utils/handlers/emailTemplates.ts
@@ -1,9 +1,13 @@
 const emailTemplates = {
   registrationConfirmationEmail(code: string) {
-    return ` <h1>Thank for your registration</h1>
-             <p>To finish registration please follow the link below:<br>
-                <a href='https://7abe02b4dcf9060ff42d9a2e3233cd31.serveo.net/auth/registration-confirmation?code=${code}'>complete registration</a>
-             </p>`
+    return ` 
+      <h1>Thank you for your registration</h1>
+      <p>To finish registration, please click the button below:</p>
+      <form action="https://7abe02b4dcf9060ff42d9a2e3233cd31.serveo.net/auth/registration-confirmation" method="POST">
+        <input type="hidden" name="code" value="${code}">
+        <button type="submit">Complete Registration</button>
+      </form>
+    `;
   },
 };
 
